538cd1f799313a5d621f24569948cc9147501b79
EazyFTW <Evan.Zimmerman@comcast.net>
2020-10-15 12:44:31 -0500

    Added last parsed time.

--- src/main/java/me/TechsCode/SpigotAPI/client/SpigotAPIClient.java
+++ src/main/java/me/TechsCode/SpigotAPI/client/SpigotAPIClient.java
@@ -1,42 +1,46 @@
 package me.TechsCode.SpigotAPI.client;
 
-import me.TechsCode.SpigotAPI.data.*;
+import me.TechsCode.SpigotAPI.data.Dataset;
 import me.TechsCode.SpigotAPI.data.lists.PurchasesList;
 import me.TechsCode.SpigotAPI.data.lists.ResourcesList;
 import me.TechsCode.SpigotAPI.data.lists.ReviewsList;
 import me.TechsCode.SpigotAPI.data.lists.UpdatesList;
 
-import java.util.*;
+import java.util.Optional;
 
 public class SpigotAPIClient extends Thread {
 
     private final DataManager dataManager;
 
-    public SpigotAPIClient(String url, String token){
+    public SpigotAPIClient(String url, String token) {
         this.dataManager = new DataManager(url, token);
     }
 
-    public Optional<Dataset> getData(){
+    public Optional<Dataset> getData() {
         return Optional.ofNullable(dataManager.getData());
     }
 
-    public long getRefreshTime(){
+    public long getLastParsed() {
+        return dataManager.getLastParsed();
+    }
+
+    public long getRefreshTime() {
         return getData().map(Dataset::getTimeCreated).orElse(0L);
     }
 
-    public ResourcesList getResources(){
+    public ResourcesList getResources() {
         return getData().map(Dataset::getResources).orElse(new ResourcesList());
     }
 
-    public UpdatesList getUpdates(){
+    public UpdatesList getUpdates() {
         return getData().map(Dataset::getUpdates).orElse(new UpdatesList());
     }
 
-    public ReviewsList getReviews(){
+    public ReviewsList getReviews() {
         return getData().map(Dataset::getReviews).orElse(new ReviewsList());
     }
 
-    public PurchasesList getPurchases(){
+    public PurchasesList getPurchases() {
         return getData().map(Dataset::getPurchases).orElse(new PurchasesList());
     }
 }
