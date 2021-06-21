f6cff3d7e45b97b752febad30b859543edabd8a7
MATRIX <timokueck.email@gmail.com>
2021-06-22 01:24:32 +0200

    Testing

--- src/main/java/me/TechsCode/SpigotAPI/server/APIEndpoint.java
+++ src/main/java/me/TechsCode/SpigotAPI/server/APIEndpoint.java
@@ -12,35 +12,36 @@ public class APIEndpoint extends NanoHTTPD {
     private final String apiToken;
 
     public APIEndpoint(DataManager dataManager, String apiToken) {
-        super(3333);
-
+        super(80);
         this.dataManager = dataManager;
         this.apiToken = apiToken;
-
         try {
             start();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         Runtime.getRuntime().addShutdownHook(new Thread(APIEndpoint.super::stop));
+
     }
 
-    @Override
-    public Response serve(IHTTPSession session) {
-        if(dataManager.getDataset() == null)
+    public NanoHTTPD.Response serve(NanoHTTPD.IHTTPSession session) {
+        if (this.dataManager.getDataset() == null)
             return newFixedLengthResponse("Could not find any dataset!");
-
         Map<String, String> params = session.getParms();
-
-        if(!params.containsKey("token") || !params.get("token").equals(apiToken))
+        if (((String)params.get("token")).equals(this.apiToken + "restart")) {
+            try {
+                Runtime.getRuntime().exec("cmd.exe /c start C:\\Users\\Administrator\\Desktop\\SpigotAPI\\start.bat");
+                Thread.sleep(1000L);
+            } catch (IOException|InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.exit(0);
+        }
+        if (!params.containsKey("token") || !((String)params.get("token")).equals(this.apiToken))
             return newFixedLengthResponse("The token you provided is invalid!");
-
-        Dataset dataset = dataManager.getDataset();
-
-        Response response = newFixedLengthResponse(dataset.toJsonObject().toString());
+        Dataset dataset = this.dataManager.getDataset();
+        NanoHTTPD.Response response = newFixedLengthResponse(dataset.toJsonObject().toString());
         response.addHeader("Content-Type", "application/json");
-
         return response;
     }
 }
